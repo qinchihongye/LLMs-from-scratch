5cc3c47848be33655d50142f062503d2ee02b1e4
qinchiHongYe <396691867@qq.com>
2024-12-18 20:26:40 +0800

    1

--- ch02/01_main-chapter-code/7_Creating_token_embeddings.py
+++ ch02/01_main-chapter-code/7_Creating_token_embeddings.py
@@ -7,3 +7,20 @@
 # @Version : 1.0
 # @Desc    :
 
+import torch
+import tiktoken
+
+input_ids = torch.tensor([2, 3, 5, 1])
+
+
+#为了简单起见，假设我们有一个只有6个单词的小词汇表，并且我们希望创建大小为3的嵌入
+vocab_size = 6
+output_dim = 3
+
+torch.manual_seed(123)
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+print(embedding_layer.weight)
+
+print(embedding_layer(torch.tensor([3])))
+
+print(embedding_layer(input_ids))
