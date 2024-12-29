00704c99a2e1af4c7695e6a84e090399695e61b5
qinchiHongYe <69714275+qinchihongye@users.noreply.github.com>
2024-12-30 00:00:20 +0800

    111

---
+++ ch04/01_main-chapter-code/4.1_4.4_Implementing a GPT model from Scratch To Generate Text.py
@@ -0,0 +1,271 @@
+from importlib.metadata import version
+
+import matplotlib
+import tiktoken
+import torch
+
+print("matplotlib version:", version("matplotlib"))
+print("torch version:", version("torch"))
+print("tiktoken version:", version("tiktoken"))
+
+
+"""  4.1 Coding an LLM architecture """
+import torch
+import torch.nn as nn
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
+
+
+
+class DummyGPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        
+        # Use a placeholder for TransformerBlock
+        self.trf_blocks = nn.Sequential(
+            *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        
+        # Use a placeholder for LayerNorm
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+
+class DummyTransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        # A simple placeholder
+
+    def forward(self, x):
+        # This block does nothing and just returns its input.
+        return x
+
+
+class DummyLayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-5):
+        super().__init__()
+        # The parameters here are just to mimic the LayerNorm interface.
+
+    def forward(self, x):
+        # This layer does nothing and just returns its input.
+        return x
+
+
+# 使用 tiktoken 分词器对由 GPT 模型的两个文本输入组成的批次进行分词
+
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+batch = []
+
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+# 初始化一个包含 1.24 亿参数的 DummyGPTModel 实例，并将 tokenized batch 输入其中。
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+
+logits = model(batch)
+print("Output shape:", logits.shape)
+print(logits)
+
+"""4.2 Normalizing activations with layer normalization"""
+# 一个小例子层归一化原理
+torch.manual_seed(123)
+
+batch_example = torch.randn(2, 5) 
+
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+print(out.shape)
+
+# 计算均值和方差
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+print("Normalized layer outputs:\n", out_norm)
+
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+torch.set_printoptions(sci_mode=False) #关闭科学计数法
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# 基于归一化思路，实现一个 `LayerNorm` 类，稍后我们可以在 GPT 模型中使用它
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
+
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+
+""" Implementing a feed forward network with GELU activations"""
+#  实现GELU
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+# 比较GELU和RELU函数
+import matplotlib.pyplot as plt
+
+gelu, relu = GELU(), nn.ReLU()
+
+# Some sample data
+x = torch.linspace(-3, 3, 100)
+y_gelu, y_relu = gelu(x), relu(x)
+
+plt.figure(figsize=(8, 3))
+for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+    plt.subplot(1, 2, i)
+    plt.plot(x, y)
+    plt.title(f"{label} activation function")
+    plt.xlabel("x")
+    plt.ylabel(f"{label}(x)")
+    plt.grid(True)
+
+plt.tight_layout()
+plt.show()
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+ffn = FeedForward(GPT_CONFIG_124M)
+
+# input shape: [batch_size, num_token, emb_size]
+x = torch.rand(2, 3, 768) 
+out = ffn(x)
+print(out.shape)
+
+"""4.4 Adding shortcut connections"""
+# 在前向代码中增加short_cut
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]), GELU())
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            # Compute the output of the current layer
+            layer_output = layer(x)
+            # Check if shortcut can be applied
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else:
+                x = layer_output
+        return x
+
+# 计算模型向后传递中的梯度的函数
+def print_gradients(model, x):
+    # Forward pass
+    output = model(x)
+    target = torch.tensor([[0.]])
+
+    # Calculate loss based on how close the target
+    # and output are
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+    
+    # Backward pass to calculate the gradients
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            # Print the mean absolute gradient of the weights
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+# 未使用shortcut
+layer_sizes = [3, 3, 3, 3, 3, 1]  
+sample_input = torch.tensor([[1., 0., -1.]])
+
+torch.manual_seed(123)
+model_without_shortcut = ExampleDeepNeuralNetwork(
+    layer_sizes, use_shortcut=False
+)
+print_gradients(model_without_shortcut, sample_input)
+
+# 使用shortcut
+torch.manual_seed(123)
+model_with_shortcut = ExampleDeepNeuralNetwork(
+    layer_sizes, use_shortcut=True
+)
+print_gradients(model_with_shortcut, sample_input)
+
+
+"""Connecting attention and linear layers in a transformer block"""
